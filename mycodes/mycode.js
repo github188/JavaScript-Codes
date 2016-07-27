84d769c8b4c125a1cd7301dc4bdc7c9ceb4b18a5
gcclll <604522194@qq.com>
2016-07-27 20:29:35 +0800

    mod findAllRightEleFromObjArray

--- mycodes/mycode.js
+++ mycodes/mycode.js
@@ -295,15 +295,17 @@ function ArrayHandler( ) {
 		var srcKeys 	= Object.keys(source);
 
 		collection.map(function (obj, index) {
-			has = srcKeys.map(function(keyV){
+			srcKeys.map(function(keyV){
 				if ( obj.hasOwnProperty(keyV) ) { // has
 					// 判断值是否相同
 					console.log(obj[keyV] + "-" + source[keyV]);
-					if ( obj[keyV] != source[keyV] ) return false;
+					if ( obj[keyV] != source[keyV] ) has = false;
 				} else {
-					return false;
+					has = false;
 				}
 			});	
+
+			console.log(has);
 
 			// 只要has为false，表示至少有一个source中的键值对不被包含在collection的对象中
 			// 则不符合条件，不添加到最后的符合要求的结果中
