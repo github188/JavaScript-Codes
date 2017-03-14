50ea5e558be4951131101630c441a3da91557fc2
gcclll <gccll.love@gmail.com>
2017-03-14 03:21:30 -0700

    add test/css/sass

---
+++ test/css/sass/scripts/hello.js
@@ -0,0 +1,8 @@
+
+var world = require( './world' );
+
+function hello( str ) {
+    console.log( 'hello ' + str );
+}
+
+hello( world );
---
+++ test/css/sass/scripts/test.js
@@ -0,0 +1,5 @@
+
+
+function hello( str ) {
+   alert( 'hello' + str ); 
+}
