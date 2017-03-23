7c03f59ef4dd21c7da55aefff680f4aeb47a519f
gcclll <604522194@qq.com>
2017-03-23 22:57:48 +0800

    add webpack

---
+++ pack-tools/webpack/firstDemo/app/index.js
@@ -0,0 +1,3 @@
+import "dist/css/app.css";
+
+
---
+++ pack-tools/webpack/firstDemo/webpack.config.js
@@ -0,0 +1,25 @@
+
+var path = require('path'),
+	ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+
+	entry: './app/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+
+	module: {
+		rules: [{
+			test: /\.css$/,
+			use: ExtractTextPlugin.extract({
+				use: 'css-loader'
+			})
+		}]
+	},
+
+	plugins: [
+		new ExtractTextPlugin('styles.css'),
+	]
+};
