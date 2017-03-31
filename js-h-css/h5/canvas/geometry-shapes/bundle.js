7fb35bcd53a9ca93343acf4a45b5895baff8615a
gcclll <gccll.love@gmail.com>
2017-03-31 03:25:22 -0700

    add h5 geometry shapes codes

--- js-h-css/h5/canvas/geometry-shapes/bundle.js
+++ js-h-css/h5/canvas/geometry-shapes/bundle.js
@@ -1,3 +1,75 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+/******/
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// identity function for calling harmony imports with the correct context
+/******/ 	__webpack_require__.i = function(value) { return value; };
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ (function(module, exports) {
+
 /**
  * 生成正多边形
  * @authors zc li (ccc_simon@163.com)
@@ -363,3 +435,52 @@ function debug( str ) {
 module.exports = function (canvas) {
     return new RegularPolygon(canvas);
 }
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+
+var polygon     = __webpack_require__(0),
+
+
+    canvas = document.getElementById('reg-polygon'),
+
+    canvasWidth = -1
+    ;
+
+canvas.width = 300;
+canvas.height = canvas.width;
+
+canvasWidth = canvas.width;
+
+var regPolygon = polygon(canvas);
+
+var result = null;
+
+// canvas.onclick = function (e) {
+    
+//     regPolygon.pause();
+// }
+
+
+
+regPolygon
+    .origin()
+    .r(60)
+    .sides(3)
+    .styles({
+        // "isStroke": true,
+        // "strokeStyle": "red",
+        "fillStyle": "red"
+        // "lineWidth": 3
+    }).draw()
+    .rotate(Math.PI / 6).draw()
+    .rotate(Math.PI / 3).draw()
+    .rotate(Math.PI / 2).draw()
+
+
+
+/***/ })
+/******/ ]);
--- js-h-css/h5/canvas/geometry-shapes/index.js
+++ js-h-css/h5/canvas/geometry-shapes/index.js
@@ -1,1 +1,40 @@
 
+
+var polygon     = require('./regular-polygon'),
+
+
+    canvas = document.getElementById('reg-polygon'),
+
+    canvasWidth = -1
+    ;
+
+canvas.width = 300;
+canvas.height = canvas.width;
+
+canvasWidth = canvas.width;
+
+var regPolygon = polygon(canvas);
+
+var result = null;
+
+// canvas.onclick = function (e) {
+    
+//     regPolygon.pause();
+// }
+
+
+
+regPolygon
+    .origin()
+    .r(60)
+    .sides(3)
+    .styles({
+        // "isStroke": true,
+        // "strokeStyle": "red",
+        "fillStyle": "red"
+        // "lineWidth": 3
+    }).draw()
+    .rotate(Math.PI / 6).draw()
+    .rotate(Math.PI / 3).draw()
+    .rotate(Math.PI / 2).draw()
+
