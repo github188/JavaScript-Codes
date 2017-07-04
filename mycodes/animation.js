2b1a68b4b93b06baa246cb141a07f53acb5533f5
lizc <gccll.love@gmail.com>
2017-07-04 11:42:20 +0800

    优化 fadeFn

--- mycodes/animation.js
+++ mycodes/animation.js
@@ -9,39 +9,85 @@
  * @param  {[type]} refV 	参考值，用来递增递减，最后值会赋值给元素的 opacity
  * @param  {[type]} opacity 元素的初始透明度
  * @return {[type]}
+ * 使用方式
+ *
+ * 	1. 淡入
+ * 	function show(cb) {
+		fadeFn(el, 'in', 0, 0, function (el, v) {
+			el.style.opacity = v;
+		}, false)();
+	}
+	
+	2. 淡出
+	function hide(cb) {
+		fadeFn(el, 'out', 100, 1, function (el, v) {
+			el.style.opacity = v;
+		}, false)();
+	}
+
+	3. 淡入淡出
+	function inout() {
+		fadeFn(el, 'in', 0, 0, function (el, v) {
+			el.style.border = '10px solid rgba(6, 127, 210, ' + v + ')';
+		}, true)();
+	}
+
+	4. 目标函数
+	function fadeBorder(el, v) {
+		// console.log('b - ' + v);
+		el.style.border = '10px solid rgba(6, 127, 210, ' + v + ')'
+	}
  */
-function fadeFn(el, inout, refV, opacity) {
+function fadeFn(el, inout, refV, opacity, handler, circle) {
 
  	var value = refV;
  	var timer = null;
 
  	if (inout === 'in') {
- 		el.style.display = 'block';
+ 		// el.style.display = 'block';
  	}
 
- 	el.style.opacity = opacity;
+ 	handler(el, opacity);
+ 	// fadeBorder(el, opacity);
+ 	// el.style.opacity = opacity;
 
- 	return function (start, end, stepV) {
+ 	return function fade(start, end, stepV) {
  		var startV = start || 0;
  		var endV = end || 100;
  		var step = stepV || 1;
  		var condition = inout === 'in' ? value >= endV : value <= startV;
+ 		var args = arguments;
+ 		// console.log(condition)
  		if (condition) {
- 			el.style.opacity = value / 100;
+ 			// el.style.opacity = value / 100;
  			if (inout === 'out') {
- 				el.style.display = 'none';
+ 				// el.style.display = 'none';
  			}
- 			clearTimeout(timer);
- 			return;
+
+ 			if (circle) {
+ 				value = refV = Math.abs(refV - 100);
+ 				opacity = Math.abs(opacity - 1);
+ 				inout = inout === 'in' ? 'out' : 'in';
+ 				fade(start, end, stepV);
+ 			} else {
+ 				clearTimeout(timer);
+ 				return;
+ 			}
  		}
 
  		value = inout === 'in' ? value + step : value - step;
- 		el.style.opacity = value / 100;
+
+ 		// el.style.opacity = value / 100;
+ 		// fadeBorder(el, value / 100);
+ 		handler(el, value / 100);
+
+ 		// console.log(el.style.opacity);
 
  		clearTimeout(timer);
  		timer = setTimeout(function () {
- 			fade(el, inout, startV, endV, step);
- 		}, 10);	
+ 			fade(start, end, stepV);
+ 		}, 15);
  	};
+
 };
 
